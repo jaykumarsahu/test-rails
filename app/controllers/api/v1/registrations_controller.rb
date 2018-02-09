e005474b8f157de8d095adc09fdb53bed409369c
Jay Sahu <jsahu.ror@gmail.com>
2018-02-09 20:35:42 +0530

    fix

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,17 +3,20 @@ class Api::V1::RegistrationsController < ApplicationController
   skip_before_action :authenticate_request!, only: [:create]
 
   def create
-    user = User.new(user_params)
-
-    if user.password == user.password_confirmation && user.save
-      msg = "User has been signed up successfully."
-      render json: {
-        status: "SUCCESS",
-        message: msg,
-        user: user.as_json(only: [:first_name, :last_name, :email, :phone, :address, :city, :state, :zip, :country])
-      }, status: :ok
-    else
-      render json: { status: "FAILED", error: user.errors.full_messages }, status: :ok
+    begin
+      user = User.new(user_params)
+      if user.save
+        msg = "User has been signed up successfully."
+        render json: {
+          status: "SUCCESS",
+          message: msg,
+          user: user.as_json(only: [:first_name, :last_name, :email, :phone, :address, :city, :state, :zip, :country])
+        }, status: :ok
+      else
+        render json: { status: "FAILED", error: user.errors.full_messages }, status: :ok
+      end
+    rescue Exception => e
+      render json: { status: "FAILED", error: e.message }, status: :ok
     end
   end
 
