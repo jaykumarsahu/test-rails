e6e0dca14c70ab318ee3a095a2e8fd93929265a6
Jay Sahu <jsahu.ror@gmail.com>
2018-02-13 16:02:18 +0530

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 user = User.new(
-      first_name: 'first_name',
-      last_name: 'last_name',
-      email: 'user@example.com',
-      phone: '12345687',
-      address: 'IT park',
-      city: 'Indore',
-      state: 'MP',
-      zip: '452001',
-      country: 'IN',
-      password: '12345678',
-      password_confirmation: '12345678'
-  )
+  first_name: 'first_name',
+  last_name: 'last_name',
+  email: 'user@example.com',
+  phone: '12345687',
+  address: 'IT park',
+  city: 'Indore',
+  state: 'MP',
+  zip: '452001',
+  country: 'IN',
+  password: '12345678',
+  password_confirmation: '12345678'
+)
 
 user.save
